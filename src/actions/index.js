60e70f4b34808e0760f6722b41bc1f206128c429
Anders Ytterström <anders.ytterstrom@kundo.se>
2019-04-23 14:58:20 +0200

    Add selected album actions and reducers
    Default state is an empty object ({}).
    SELECT_ALBUM will update state with an album.
    UNSELECT_ALBUM resets state to default state.

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,8 @@ export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
 export const SET_SORT_KEY = 'SET_SORT_KEY';
 export const LOAD_ALBUMS = 'LOAD_ALBUMS';
 export const LOAD_ALBUMS_OK = 'LOAD_ALBUMS_OK';
+export const SELECT_ALBUM = 'SELECT_ALBUM';
+export const UNSELECT_ALBUM = 'UNSELECT_ALBUM';
 
 export const setVisibilityFilter = filter => ({
     type: SET_VISIBILITY_FILTER,
@@ -23,3 +25,14 @@ export const albumsLoadedOk = albums => ({
         albums,
     },
 })
+
+export const selectAlbum = album => ({
+    type: SELECT_ALBUM,
+    payload: {
+        album
+    },
+});
+
+export const unselectAlbum = () => ({
+    type: UNSELECT_ALBUM,
+});
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import albums from './albums';
 import visibilityFilter from './visibility-filter';
 import sortKey from "./sort-key";
+import selectedAlbum from './selected-album';
 
 export default combineReducers({
     albums,
     visibilityFilter,
     sortKey,
+    selectedAlbum,
 });
